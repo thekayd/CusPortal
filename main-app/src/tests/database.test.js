d1f60656898a4c0be2c3fb42e611f26b9d3eb8d9
Daniel Akilo <kayodeakilo1@gmail.com>
2024-10-08 21:49:42 +0200

    Fixed Database.test.js

--- main-app/src/tests/database.test.js
+++ main-app/src/tests/database.test.js
@@ -1,25 +1,32 @@
-// src/tests/database.test.js
+require('dotenv').config();  // Make sure this is at the top
+
 if (typeof TextEncoder === 'undefined') {
-    global.TextEncoder = require('util').TextEncoder;  // Using Node.js built-in TextEncoder
-  }
-  
-  if (typeof TextDecoder === 'undefined') {
-    global.TextDecoder = require('util').TextDecoder;  // Using Node.js built-in TextDecoder
-  }
-  
-  const mongoose = require('mongoose');
-  
-  describe('Database Connection', () => {
-    beforeAll(async () => {
-      await mongoose.connect(process.env.MONGODB_URI);
-    });
-  
-    afterAll(async () => {
-      await mongoose.connection.close();
-    });
-  
-    it('should connect to the database', () => {
-      expect(mongoose.connection.readyState).toBe(1);
-    });
+  global.TextEncoder = require('util').TextEncoder;  // Using Node.js built-in TextEncoder
+}
+
+if (typeof TextDecoder === 'undefined') {
+  global.TextDecoder = require('util').TextDecoder;  // Using Node.js built-in TextDecoder
+}
+
+const mongoose = require('mongoose');
+
+describe('Database Connection', () => {
+  beforeAll(async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    } catch (error) {
+      console.error('Error connecting to the database', error);
+    }
   });
-  
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  it('should connect to the database', () => {
+    expect(mongoose.connection.readyState).toBe(1);  // 1 means connected
+  });
+});
