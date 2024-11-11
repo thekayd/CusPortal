6a0d20ab4c60abadf8b5bcef43eb6213dea45519
Daniel Akilo <kayodeakilo1@gmail.com>
2024-11-11 15:00:57 +0200

    Set up GitHub Actions for SonarCloud

--- main-app/jest.config.js
+++ main-app/jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/*.test.ts'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/*.test.ts'],
+  
+  // Add these lines for coverage
+  collectCoverage: true, // Enable code coverage collection
+  coverageDirectory: './coverage', // Directory to output coverage reports
+  coverageReporters: ['lcov', 'json', 'text'], // Formats SonarCloud can read
+};
