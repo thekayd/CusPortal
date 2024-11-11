a986c288aa390ec4e1c40ccd5822b71c6349201f
Daniel Akilo <kayodeakilo1@gmail.com>
2024-11-11 15:16:25 +0200

    reverted back to Charle's code

--- main-app/jest.config.js
+++ main-app/jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
-  
-  // Add these lines for coverage
-  collectCoverage: true, // Enable code coverage collection
-  coverageDirectory: './coverage', // Directory to output coverage reports
-  coverageReporters: ['lcov', 'json', 'text'], // Formats SonarCloud can read
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/*.test.ts'],
+  };
